e61adde39dd8c0faebad7de260c68610f6e4c9e8
tenric <minjieni@vip.qq.com>
2014-05-23 21:29:01 +0800

    fix bug
    CGContextSetStrokeColorWithColor: invalid context 0x0. This is a
    serious error. This application, or a library it uses, is using an
    invalid context  and is thereby contributing to an overall degradation
    of system stability and reliability. This notice is a courtesy: please
    fix this problem. It will become a fatal error in an upcoming update.

--- MJPasswordView/mjpasswordview/MJPathLayer.m
+++ MJPasswordView/mjpasswordview/MJPathLayer.m
@@ -37,7 +37,12 @@
    
     point = self.passwordView.previousTouchPoint;
     CGContextAddLineToPoint(ctx, point.x, point.y);
-    [self.passwordView.pathColour setStroke];
+// 下面这行的作用是将当前context的strokeColor设成pathColour，但是当前context是null
+// 你可以调用UIGraphicsGetCurrentContext()看看返回值（就是当前context）是否为空，
+// 删除掉下面这行就ok了，原因有二：
+// 1、上面已经用CGContextSetStrokeColor设置过颜色了，没必要重新设一次
+// 2、setStroke时UIKit的方法，默认会调用UIGraphicsGetCurrentContext(),而UIGraphicsGetCurrentContext()只能在UIView的drawRect方法里面调用，这里调用是错误的。
+//    [self.passwordView.pathColour setStroke];
     CGContextDrawPath(ctx, kCGPathStroke);
 }
 
